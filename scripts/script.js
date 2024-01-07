ce84cbc224147bf6a0c1e74741ce758676fc670d
Эрнест <101067653+Nexoner@users.noreply.github.com>
2024-01-07 12:14:20 +0300

    add scroll on teacher section

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Добавь скрипт после загрузки DOM
+document.addEventListener('DOMContentLoaded', function () {
+  // Получаем элементы
+  const container = document.querySelector('.teachers_list')
+
+  // Добавляем обработчик события для отслеживания изменений в скролле
+  container.addEventListener('scroll', function () {
+    // Тут ты можешь добавить дополнительные действия при скролле, если это нужно
+  })
+})
